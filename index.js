df9a5082c9be52880204bbcbcc242fd182d43f7c
Phillipe Rodriguez <philliperod@gmail.com>
2020-08-31 08:44:49 -0500

    updated dice class and added JS function for random roll with eventListener

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function rollingDice() {
-  var randomRoll = Math.floor(Math.random() * 6) + 1;
-  console.log('This is your roll: ' + randomRoll);
-}
+window.addEventListener('click', () => {
+  var randomRoll_1 = Math.floor(Math.random() * 6) + 1;
+  var randomRoll_2 = Math.floor(Math.random() * 6) + 1;
+
+  var dye1 = 'dist/css/images/dice' + randomRoll_1 + '.png';
+  var dye2 = 'dist/css/images/dice' + randomRoll_2 + '.png';
+
+  document.querySelector('.dice1').setAttribute('src', dye1);
+  document.querySelector('.dice2').setAttribute('src', dye1);
+});
