5f45ed5eebadd3e57dfb8377dd0c0a280c893b4b
Phillipe Rodriguez <philliperod@gmail.com>
2020-08-31 09:46:35 -0500

    reduce h1 sizing and applied logic to randomROll in JS

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ window.addEventListener('click', () => {
   var dye2 = 'dist/css/images/dice' + randomRoll_2 + '.png';
 
   document.querySelector('.dice1').setAttribute('src', dye1);
-  document.querySelector('.dice2').setAttribute('src', dye1);
+  document.querySelector('.dice2').setAttribute('src', dye2);
+
+  if (randomRoll_1 > randomRoll_2) {
+    document.querySelector('h1').innerHTML = 'Player One Wins!';
+  } else if (randomRoll_1 < randomRoll_2) {
+    document.querySelector('h1').innerHTML = 'Plauyer Two Wins!';
+  } else {
+    document.querySelector('h1').innerHTML = "It's A Tie!";
+  }
 });
